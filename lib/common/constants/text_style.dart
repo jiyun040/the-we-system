9106a9d4d2b1e1498f89008451d1eeb4998c68a0
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-06-29 21:48:09 +0900

    design: 텍스트스타일 크기 키우기

--- lib/common/constants/text_style.dart
+++ lib/common/constants/text_style.dart
@@ -3,27 +3,27 @@ import 'package:the_we_system/common/constants/color.dart';
 
 class TheWeTextStyle {
   static TextStyle title = defaultTextStyle.copyWith(
-    fontSize: 20,
+    fontSize: 22,
     fontWeight: FontWeight.w700,
   );
 
   static TextStyle pageTitle = defaultTextStyle.copyWith(
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: FontWeight.w700,
   );
 
   static TextStyle subtitle = defaultTextStyle.copyWith(
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: FontWeight.w700,
   );
 
   static TextStyle makeApproval = defaultTextStyle.copyWith(
-    fontSize: 15,
+    fontSize: 16,
     fontWeight: FontWeight.w600,
   );
 
   static TextStyle section = defaultTextStyle.copyWith(
-    fontSize: 12,
+    fontSize: 13,
     fontWeight: FontWeight.w700,
   );
 
@@ -33,17 +33,17 @@ class TheWeTextStyle {
   );
 
   static TextStyle body = defaultTextStyle.copyWith(
-    fontSize: 13,
+    fontSize: 15,
     fontWeight: FontWeight.w500,
   );
 
   static TextStyle caption = defaultTextStyle.copyWith(
-    fontSize: 11,
+    fontSize: 13,
     fontWeight: FontWeight.w500,
   );
 
   static TextStyle metric = defaultTextStyle.copyWith(
-    fontSize: 28,
+    fontSize: 30,
     fontWeight: FontWeight.w700,
   );
 }
